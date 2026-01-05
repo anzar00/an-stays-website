748bfb291e0fd3d785e7b1500511b5e0d97cfa7e
Anzar <peerzada.anzar@gmail.com>
2026-01-06 03:01:35 +0530

    fix router links

--- src/app/pages/home/accommodations/accommodations.component.ts
+++ src/app/pages/home/accommodations/accommodations.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ACCOMMODATIONS } from '../../../core/data/accommodations.data';
 import { Accommodation } from '../../../core/models/accommodation.model';
 
 @Component({
   selector: 'app-accommodations',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './accommodations.component.html',
 })
 export class AccommodationsComponent {
