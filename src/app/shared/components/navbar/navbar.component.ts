577829ba1ed674b324c0a43bd2025ecafb4a1078
Anzar <peerzada.anzar@gmail.com>
2026-01-06 03:31:26 +0530

    fix router links

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -10,11 +12,24 @@ import { RouterLink } from '@angular/router';
 export class NavbarComponent {
   mobileMenuOpen = false;
 
+  constructor(private router: Router, private viewport: ViewportScroller) {}
+  
   toggleMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
   }
 
   closeMenu() {
     this.mobileMenuOpen = false;
+  }
+
+  scrollToAccommodations(event: Event) {
+    event.preventDefault();
+    const scroll = () => this.viewport.scrollToAnchor('accommodations');
+    if (this.router.url !== '/') {
+      this.router.navigateByUrl('/').then(() => setTimeout(scroll));
+    } else {
+      scroll();
+    }
+    this.closeMenu();
   }
 }
