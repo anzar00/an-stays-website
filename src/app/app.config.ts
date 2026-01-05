900ff70385ded4f824bc551963838089b28a270a
Anzar <peerzada.anzar@gmail.com>
2026-01-06 03:17:30 +0530

    fix router links

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(
+      routes, 
+      withHashLocation(),
+      withInMemoryScrolling({anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'})
+    ), 
+    provideClientHydration(withEventReplay())]
 };
