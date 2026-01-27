0d81d2461b3b5c8ad8ff02c081973a42d4e414fc
Peerzada Anzar Azmat <32608101+anzar00@users.noreply.github.com>
2026-01-28 01:41:48 +0530

    Update team.data.ts

--- src/app/core/data/team.data.ts
+++ src/app/core/data/team.data.ts
@@ -4,33 +4,33 @@ export const TEAM_MEMBERS: TeamMember[] = [
   {
     name: 'Mahaboob Basha',
     role: 'CEO | CFO',
-    image: '/assets/images/team/placeholder.webp',
+    image: 'assets/images/team/placeholder.webp',
   },
   {
     name: 'Beebi Jaan',
     role: 'COO',
-    image: '/assets/images/team/placeholder.webp',
+    image: 'assets/images/team/placeholder.webp',
   },
   {
     name: 'Aamna Nafiza',
     role: 'CMO',
-    image: '/assets/images/team/placeholder.webp',
+    image: 'assets/images/team/placeholder.webp',
   },
   {
     name: 'Shaik Shamsheer',
     role: 'Managing Director',
     organization: 'AN Royal',
-    image: '/assets/images/team/placeholder.webp',
+    image: 'assets/images/team/placeholder.webp',
   },
   {
     name: 'Shaik Shabbir',
     role: 'Managing Director',
     organization: 'AN Royal Stays PG & INN',
-    image: '/assets/images/team/placeholder.webp',
+    image: 'assets/images/team/placeholder.webp',
   },
   {
     name: 'Divya T',
     role: 'Full Time Intern',
-    image: '/assets/images/team/placeholder.webp',
+    image: 'assets/images/team/placeholder.webp',
   },
 ];
